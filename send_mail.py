0404c61e2c8d9e3de926f69763d373d87a061562
aditheawesome <44825213+aditheawesome@users.noreply.github.com>
2020-08-21 14:32:41 -0400

    Trying to fix bug

--- send_mail.py
+++ send_mail.py
@@ -9,22 +9,20 @@ def send_mail(receiver, link):
         api_key = os.environ.get('SENDGRID_API_KEY')
     else:
         api_key = process.env.SENDGRID_API_KEY
-
-
-    sender = "no-reply@vidveda.com"
-    message = Mail(
-        from_email=(sender,"VID VEDA") ,
-        to_emails=receiver,
-        subject='TEST MAIL',
-        html_content=f'<strong>Hi {receiver}! The link is {link} </strong>')
-    try:
-        sg = SendGridAPIClient(api_key)
-        response = sg.send(message)
-        print(response.status_code)
-        print(response.body)
-        print(response.headers)
-        flash('Email Sent')
-        return redirect('/forgot')
-    except Exception as e:
-        flash('Sorry, but the email linked to this account is invalid')
-        return redirect('/forgot')
+        sender = "no-reply@vidveda.com"
+        message = Mail(
+            from_email=(sender,"VID VEDA") ,
+            to_emails=receiver,
+            subject='TEST MAIL',
+            html_content=f'<strong>Hi {receiver}! The link is {link} </strong>')
+        try:
+            sg = SendGridAPIClient(api_key)
+            response = sg.send(message)
+            print(response.status_code)
+            print(response.body)
+            print(response.headers)
+            flash('Email Sent')
+            return redirect('/forgot')
+        except Exception as e:
+            flash('Sorry, but the email linked to this account is invalid')
+            return redirect('/forgot')
