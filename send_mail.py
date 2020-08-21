b93afe9a9277661a345f954bc057ec32c0d9ca9f
aditheawesome <44825213+aditheawesome@users.noreply.github.com>
2020-08-21 16:33:38 -0400

    Update send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -4,7 +4,6 @@ from sendgrid.helpers.mail import Mail
 from boto.s3.connection import S3Connection
 from flask import Flask, render_template, url_for, request, redirect, flash
 def send_mail(receiver, link):
-    return 'hi'
     env = "pro"
     
     if env == "dev" :
@@ -17,14 +16,10 @@ def send_mail(receiver, link):
             to_emails=receiver,
             subject='TEST MAIL',
             html_content=f'<strong>Hi {receiver}! The link is {link} </strong>')
-        try:
-            sg = SendGridAPIClient(api_key)
-            response = sg.send(message)
-            flash('Email Sent')
-            return redirect('/forgot')
-            print(response.status_code)
-            print(response.body)
-            print(response.headers)
-        except Exception as e:
-            flash('Sorry, but the email linked to this account is invalid')
-            return redirect('/forgot')
+        sg = SendGridAPIClient(api_key)
+        response = sg.send(message)
+        flash('Email Sent')
+        return redirect('/forgot')
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
