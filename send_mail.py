98196569660ccab9663c40d8a4362109b5430c22
aditheawesome <44825213+aditheawesome@users.noreply.github.com>
2020-08-21 16:36:55 -0400

    Update send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -4,22 +4,17 @@ from sendgrid.helpers.mail import Mail
 from boto.s3.connection import S3Connection
 from flask import Flask, render_template, url_for, request, redirect, flash
 def send_mail(receiver, link):
-    env = "pro"
-    
-    if env == "dev" :
-        api_key = os.environ.get('SENDGRID_API_KEY')
-    else:
-        api_key = S3Connection(os.environ['SENDGRID_API_KEY'])
-        sender = "no-reply@vidveda.com"
-        message = Mail(
-            from_email=(sender,"VID VEDA") ,
-            to_emails=receiver,
-            subject='TEST MAIL',
-            html_content=f'<strong>Hi {receiver}! The link is {link} </strong>')
-        sg = SendGridAPIClient(api_key)
-        response = sg.send(message)
-        flash('Email Sent')
-        return redirect('/forgot')
-        print(response.status_code)
-        print(response.body)
-        print(response.headers)
+    api_key = S3Connection(os.environ['SENDGRID_API_KEY'])
+    sender = "no-reply@vidveda.com"
+    message = Mail(
+        from_email=(sender,"VID VEDA") ,
+        to_emails=receiver,
+        subject='TEST MAIL',
+        html_content=f'<strong>Hi {receiver}! The link is {link} </strong>')
+    sg = SendGridAPIClient(api_key)
+    response = sg.send(message)
+    flash('Email Sent')
+    return redirect('/forgot')
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
