46da50936d1c5d0c4fe17bd2131403273dbd9055
aditheawesome <44825213+aditheawesome@users.noreply.github.com>
2020-08-21 16:41:12 -0400

    Update send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -4,17 +4,25 @@ from sendgrid.helpers.mail import Mail
 from boto.s3.connection import S3Connection
 from flask import Flask, render_template, url_for, request, redirect, flash
 def send_mail(receiver, link):
-    api_key = S3Connection(os.environ['SENDGRID_API_KEY'])
-    sender = "no-reply@vidveda.com"
-    message = Mail(
-        from_email=(sender,"VID VEDA") ,
-        to_emails=receiver,
-        subject='TEST MAIL',
-        html_content=f'<strong>Hi {receiver}! The link is {link} </strong>')
-    sg = SendGridAPIClient(api_key)
-    response = sg.send(message)
-    flash('Email Sent')
-    return redirect('/forgot')
-    print(response.status_code)
-    print(response.body)
-    print(response.headers)
+    env = "pro"
+
+    if env == "dev" :
+        api_key = os.environ.get('SENDGRID_API_KEY')
+    else:
+        sender = 'no-reply@vidveda.com'
+        message = Mail(
+            from_email=(sender,"VID VEDA") ,
+            to_emails=receiver,
+            subject='TEST MAIL',
+            html_content=f'<strong>Hi {receiver}! The link is {link} </strong>')
+        try:
+            sg = SendGridAPIClient(api_key)
+            response = sg.send(message)
+            flash('Email Sent')
+            return redirect('/forgot')
+            print(response.status_code)
+            print(response.body)
+            print(response.headers)
+        except Exception as e:
+            flash('Sorry, but the email linked to this account is invalid')
+            return redirect('/forgot')
