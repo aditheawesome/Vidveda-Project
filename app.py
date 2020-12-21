567513b5160502f1e32dc41ccbea7dafaaeae537
aditheawesome <44825213+aditheawesome@users.noreply.github.com>
2020-12-21 15:12:56 -0500

    trying to use test.db instead

--- app.py
+++ app.py
@@ -46,8 +46,8 @@ if env == 'dev':
     #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
     #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
-    #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://bvipgytqmvgyiq:7b6c0f324808e528c01d5e2351c52834e94cdb8fc92cdd3e268a0977a8ca2d84@ec2-54-235-192-146.compute-1.amazonaws.com:5432/dfmrk83fdrmlua'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://bvipgytqmvgyiq:7b6c0f324808e528c01d5e2351c52834e94cdb8fc92cdd3e268a0977a8ca2d84@ec2-54-235-192-146.compute-1.amazonaws.com:5432/dfmrk83fdrmlua'
 
 else:
     app.debug = False
